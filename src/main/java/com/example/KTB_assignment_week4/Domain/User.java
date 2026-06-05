6535d5771fd61c66d878cb2597f0ae4292afd572
rndnjsgud <00kwh@naver.com>
2026-06-05 23:06:29 +0900

    feat: 사용자 도메안 클래스 추가

---
+++ src/main/java/com/example/KTB_assignment_week4/Domain/User.java
@@ -0,0 +1,10 @@
+package com.example.KTB_assignment_week4.Domain;
+
+public class User {
+    Long userId;        //사용자 식별용 id값, UUID 사용
+    String nickname;    //닉네임
+    String email;       //로그인 아이디
+    String passWord;    //비밀번호(일단 평문으로 저장)
+    UserRole userRole;  //사용자 권한 구분(사용자, 어드민)
+    Boolean isDeleted;  //사용자 탈퇴 여부(소프트 delete)
+}
---
+++ src/main/java/com/example/KTB_assignment_week4/Domain/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.KTB_assignment_week4.Domain;
+
+public enum UserRole {
+        USER,
+        ADMIN
+}
